be46e4bca35ce50e85937ba4a089b0bfdafcf4d6
Rahul Sharma <rahulsharma.cse00@gmail.com>
2024-02-22 22:20:54 +0530

    Query Selector

--- DOM-1/script.js
+++ DOM-1/script.js
@@ -7,15 +7,19 @@ console.log("Hello Dom");
 // console.dir(document.body);
 
 // let heading = document.getElementById("heading");
-// console.log(heading);
 // console.dir(heading);
+// console.log(heading);
 
 
-let header= document.getElementsByClassName("heading");
-console.log(header);
+// let header= document.getElementsByClassName("heading");
+// console.dir(header);
 
 
+// let para =document.getElementsByName("p");
+// console.dir(para);
 
+let elements = document.querySelector("p");
+console.dir(elements);
 
 
 
