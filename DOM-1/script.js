ef718e1136487af50800330649a453acbe45967a
Rahul Sharma <rahulsharma.cse00@gmail.com>
2024-02-22 21:55:59 +0530

    ID Class Dom

--- DOM-1/script.js
+++ DOM-1/script.js
@@ -2,11 +2,17 @@ console.log("Hello Dom");
 
 // alert("hello Dom");
 
-console.dir(window.document);
-console.dir(document);
-console.dir(document.body);
+// console.dir(window.document);
+// console.dir(document);
+// console.dir(document.body);
+
+// let heading = document.getElementById("heading");
+// console.log(heading);
+// console.dir(heading);
 
 
+let header= document.getElementsByClassName("heading");
+console.log(header);
 
 
 
